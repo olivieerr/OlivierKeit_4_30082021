1955e94f82466f0976d92e23b18b04a59a2b83a7
olivieerr <olivieerr@site-en-vrac.com>
2021-09-01 16:34:12 +0200

    modifiaction du formulaire

--- js/formHandler.js
+++ js/formHandler.js
@@ -1,5 +1,5 @@
 $(function()
-{	
+{
 	$("input,textarea").jqBootstrapValidation(
     {
      	preventSubmit: true,
@@ -40,9 +40,9 @@ $(function()
 		    		cache: false,
 		    		success: function() // Success
 		 			{  
-						if($form.is('[success-msg]')) // Show Success Message
+						if($form.is('Your message has been sent.')) // Show Success Message '[success-msg]'
 						{
-							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('success-msg')+"</strong></div></div>");
+							$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('Your message has been sent.')+"</strong></div></div>");
 						}
 						else // Re-Direct
 						{
